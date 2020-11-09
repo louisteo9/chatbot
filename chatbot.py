7929abff0e4ca70a67e8811478ebc0907e2bc948
Louis Teo <47262286+louisteo9@users.noreply.github.com>
2020-11-09 15:31:36 +0800

    update scripts

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,7 @@
 from chatterbot import ChatBot
 
 bot = ChatBot('Buddy',
-            storage_adapter='chatterbot.storage.SQLStorageAdapter',
-            database_uri='sqlite:///database.sqlite3_eng',
-            logic_adapters=[
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.BestMatch',
-        {
+            logic_adapters=[{
             'import_path': 'chatterbot.logic.BestMatch',
             'default_response': 'I am sorry, but I do not understand.',
             'maximum_similarity_threshold': 0.90
